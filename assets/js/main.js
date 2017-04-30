e0649a30633b9a560c821325a56460af6a407ffa
pkj <mepankaj@live.in>
2017-04-30 19:31:41 -0400

    Code clean up

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,156 +1,72 @@
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+//Variable Declarations//
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+var entryTemp =  '<div class="item row"><div class="desc col-md-12 col-sm-12 col-xs-12"><h3 class="title"><a href="{url}" target="_blank">{title}</a></h3></div></div><hr class="divider" />';
+var temphold = '<p>{shortBodyPlain}</p><a class="more-link" href="{url}" target="_blank">Read more <i class="fa fa-long-arrow-right"></i></a>';
+
+var booksFeeds = ['Apple', 'Banana'];
+var startupsFeeds = ['Apple', 'Banana'];
+var fitnessFeeds = ['Apple', 'Banana'];
+var j = 0;
+var feedCount = 5;
+var url = "feedSources.json";
+var jsonData;
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+//Execution starts after document has fully loaded//
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+
 jQuery(document).ready(function($) {
-   
-    var entryTemp =  '<div class="item row"><div class="desc col-md-12 col-sm-12 col-xs-12"><h3 class="title"><a href="{url}" target="_blank">{title}</a></h3></div></div><hr class="divider" />';
-    var temphold = '<p>{shortBodyPlain}</p><a class="more-link" href="{url}" target="_blank">Read more <i class="fa fa-long-arrow-right"></i></a>';
-    var feedCount = 5;
     
-    var indiaFeeds = ['http://timesofindia.indiatimes.com/rssfeedstopstories.cms', 'http://feeds.feedburner.com/ndtvnews-top-stories', 'http://www.dnaindia.com/syndication/rss,catID-0.xml', 'http://feeds.reuters.com/reuters/INtopNews', 'http://indianexpress.com/print/front-page/feed/'];
-    var techFeeds = ['http://www.techradar.com/rss', 'https://www.nytimes.com/svc/collections/v1/publish/https://www.nytimes.com/section/technology/rss.xml','https://www.wired.com/feed/','https://www.cnet.com/rss/news/','https://news.ycombinator.com/rss','http://www.digitaltrends.com/home/feed/'];
-    var booksFeeds = ['Apple', 'Banana'];
-    var startupsFeeds = ['Apple', 'Banana'];
-    var fitnessFeeds = ['Apple', 'Banana'];
-    
-    
-     //Random 5 stories
-    function randomStories (rssDiv){
+    //Get data from JSON file
+    $.getJSON(url, function(dataFromJsonFile){
+        jsonData = dataFromJsonFile;
         
-        var headline = $(rssDiv).find('h3');
+        //step 1: Load default feed
+        loadDefaultFeeds(jsonData);
         
-        //headline.prepend('<div class="item row"><div class="desc col-md-12 col-sm-12 col-xs-12"><h3 class="title">');
-        //headline.append('</h3></div></div><hr class="divider" />');
-        
-        $("#rss-feed0").append('<div class="item row"><div class="desc col-md-12 col-sm-12 col-xs-12"><h3 class="title">' + headline.html() + '</h3></div></div><hr class="divider" />');
-        
-    }
-    var j = 0;
-    //India feed loads by default
-    for ( var i = 0; i < indiaFeeds.length; i++ ) {
-        
-        j = i+1;
-        //Empty previous feeds
-        $("#rss-feed"+j).find("div").html("");
-        $("#rss-feed"+j).find("h2").remove();
-        
-        console.log("#rss-feed"+j);
-        var populateRssFeed = function(){
-            $("#rss-feed"+j).rss(
+        //step 2: Remove previous content
+        clearPriorContent();
 
-                //Change this to your own rss feeds
-                indiaFeeds[i],
-
-                {
-                // how many entries do you want?
-                // default: 4
-                // valid values: any integer
-                limit: feedCount,
-
-                // the effect, which is used to let the entries appear
-                // default: 'show'
-                // valid values: 'show', 'slide', 'slideFast', 'slideSynced', 'slideFastSynced'
-                effect: 'slideFastSynced',
-
-                // outer template for the html transformation
-                // default: "<ul>{entries}</ul>"
-                // valid values: any string
-                layoutTemplate: "<div class=''>{entries}</div>",
-
-                // inner template for each entry
-                // default: '<li><a href="{url}">[{author}@{date}] {title}</a><br/>{shortBodyPlain}</li>'
-                // valid values: any string
-                entryTemplate: entryTemp
-                }
-            );
-        };
-        
-        parseFeed(indiaFeeds[i], '#rss-feed'+j);
-        populateRssFeed();
-    }
-    
-    //Collect latest headline from each feed
-    setTimeout(function(){ 
-        for ( var i = 1; i < indiaFeeds.length + 1; i++ ){
-            $('#rss-feed'+i).find('h3').on('load', randomStories('#rss-feed'+i));
-        }                     
-    }, 2000);
-   
+        //step 3: populate the latest stories block
+        populateLatestStoriesBlock(jsonData.indiaRssFeeds.length);
+    });
     
     $('body').on('click', '#india', function() {
-        for ( var i = 0; i < indiaFeeds.length; i++ ) {
+       //Get data from JSON file
+        $.getJSON(url, function(dataFromJsonFile){
+            jsonData = dataFromJsonFile;
+
+            //step 1: Load default feed
+            populateTopicFeed(jsonData.indiaRssFeeds.length, jsonData, "india");
             
-            //Empty previous feeds
-            $("#rss-feed"+i).find("div").html("");
-            $("#rss-feed"+i).find("h2").remove();
+            //step 2: Remove previous content
+            clearPriorContent();
             
-            //Populate Headings
-            parseFeed(indiaFeeds[i], '#rss-feed'+i);
-            
-            $("#rss-feed"+i).rss(
-
-                //Change this to your own rss feeds
-                indiaFeeds[i],
-
-                {
-                // how many entries do you want?
-                // default: 4
-                // valid values: any integer
-                limit: feedCount,
-
-                // the effect, which is used to let the entries appear
-                // default: 'show'
-                // valid values: 'show', 'slide', 'slideFast', 'slideSynced', 'slideFastSynced'
-                effect: 'slideFastSynced',
-
-                // outer template for the html transformation
-                // default: "<ul>{entries}</ul>"
-                // valid values: any string
-                layoutTemplate: '<div class="">{entries}</div>',
-
-                // inner template for each entry
-                // default: '<li><a href="{url}">[{author}@{date}] {title}</a><br/>{shortBodyPlain}</li>'
-                // valid values: any string
-                entryTemplate: entryTemp
-                }
-            );
-        }
+            //step 3: populate the latest stories block
+            populateLatestStoriesBlock(jsonData.indiaRssFeeds.length, jsonData.indiaRssFeeds);
+        });
     });
     
     $('body').on('click', '#tech', function() {
-        for ( var i = 0; i < techFeeds.length; i++ ) {
+        //Get data from JSON file
+        $.getJSON(url, function(dataFromJsonFile){
+            jsonData = dataFromJsonFile;
+
+            //step 1: Load default feed
+            populateTopicFeed(jsonData.indiaRssFeeds.length, jsonData, "tech");
             
-            //Empty previous feeds
-            $("#rss-feed"+i).find("div").html("");
-            $("#rss-feed"+i).find("h2").remove();
+            //step 2: Remove previous content
+            clearPriorContent();
             
-            parseFeed(techFeeds[i], '#rss-feed'+i);
-            
-            $("#rss-feed"+i).rss(
-
-                //Change this to your own rss feeds
-                techFeeds[i],
-
-                {
-                // how many entries do you want?
-                // default: 4
-                // valid values: any integer
-                limit: feedCount,
-
-                // the effect, which is used to let the entries appear
-                // default: 'show'
-                // valid values: 'show', 'slide', 'slideFast', 'slideSynced', 'slideFastSynced'
-                effect: 'slide',
-
-                // outer template for the html transformation
-                // default: "<ul>{entries}</ul>"
-                // valid values: any string
-                layoutTemplate: "<div class=''>{entries}</div>",
-
-                // inner template for each entry
-                // default: '<li><a href="{url}">[{author}@{date}] {title}</a><br/>{shortBodyPlain}</li>'
-                // valid values: any string
-                entryTemplate: entryTemp
-                }
-            );
-        }
+            //step 3: populate the latest stories block
+            populateLatestStoriesBlock(jsonData.indiaRssFeeds.length, jsonData.indiaRssFeeds);
+        });
     });
     
     $('body').on('click', '#books', function() {
@@ -165,17 +81,15 @@ jQuery(document).ready(function($) {
         alert( "Handler for .click() called." );
     });
     
-    $('body').on('click', '#fitness', function () {
-        console.log("yeahhhh!!! but this doesn't work for me :(");
-    });
-    
 });
 
 /////////////////////////////////////////////////////////////////////////////////////////////
 
-// YQL: https://developer.yahoo.com/yql/
-// YQL Console: https://developer.yahoo.com/yql/console/
+//Function Definitions//
 
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+//Returns feed source title
 function parseFeed(url, container) {
     
     // yql query
@@ -205,9 +119,154 @@ function parseFeed(url, container) {
     });
 }
 
+//Gather latest 5 stories
+function gatherLatestStories (rssDiv){
 
+    var headline = $(rssDiv).find('h3');
+    
+    $("#rss-feed0").append('<div class="item row"><div class="desc col-md-12 col-sm-12 col-xs-12"><h3 class="title">' + headline.html() + '</h3></div></div><hr class="divider" />');
 
+}
 
+//Populate latest headline from each feed
+function populateLatestStoriesBlock(topicFeedLength){
+    
+    setTimeout(function(){ 
+        for ( var i = 1; i < topicFeedLength; i++ ){
+            $('#rss-feed'+i).find('h3').on('load', gatherLatestStories('#rss-feed'+i));
+        }                     
+    }, 2000);
+}
+
+//India feed loads by default
+function loadDefaultFeeds(jsonData) {
+    for ( var i = 0; i < jsonData.indiaRssFeeds.length; i++ ) {
+
+        j = i+1;
+        //Empty previous feeds
+        $("#rss-feed"+j).find("div").html("");
+        $("#rss-feed"+j).find("h2").remove();
+
+        console.log("#rss-feed"+j);
+        var populateRssFeed = function(){
+            $("#rss-feed"+j).rss(
+
+                //Change this to your own rss feeds
+                jsonData.indiaRssFeeds[i],
+
+                {
+                // how many entries do you want?
+                // default: 4
+                // valid values: any integer
+                limit: feedCount,
+
+                // the effect, which is used to let the entries appear
+                // default: 'show'
+                // valid values: 'show', 'slide', 'slideFast', 'slideSynced', 'slideFastSynced'
+                effect: 'slideFastSynced',
+
+                // outer template for the html transformation
+                // default: "<ul>{entries}</ul>"
+                // valid values: any string
+                layoutTemplate: "<div class=''>{entries}</div>",
+
+                // inner template for each entry
+                // default: '<li><a href="{url}">[{author}@{date}] {title}</a><br/>{shortBodyPlain}</li>'
+                // valid values: any string
+                entryTemplate: entryTemp
+                }
+            );
+        };
+
+        parseFeed(jsonData.indiaRssFeeds[i], '#rss-feed'+j);
+        populateRssFeed();
+    }
+} 
+
+//Get Feed Addresses By Topic
+function getFeedsByTopic(topicFeedLength, jsonData, topic){
+    var feeds = [];
+    
+    if(topic === "india"){
+        for (var i = 0; i < topicFeedLength; i++){
+            feeds[i] = jsonData.indiaRssFeeds[i];
+        }
+    }else if(topic === "tech"){
+        for (var i = 0; i < topicFeedLength; i++){
+            feeds[i] = jsonData.techRssFeeds[i];
+        }
+    }else if(topic === "books"){
+        for (var i = 0; i < topicFeedLength; i++){
+            feeds[i] = jsonData.booksRssFeeds[i];
+        }
+    }else if(topic === "startups"){
+        for (var i = 0; i < topicFeedLength; i++){
+            feeds[i] = jsonData.startupsRssFeeds[i];
+        }
+    }else if(topic === "fitness"){
+        for (var i = 0; i < topicFeedLength; i++){
+            feeds[i] = jsonData.fitnessRssFeeds[i];
+        }
+    }else if(topic === "food"){
+        for (var i = 0; i < topicFeedLength; i++){
+            feeds[i] = jsonData.foodRssFeeds[i];
+        }
+    }
+    
+    return feeds;
+}
+
+//Generic Populate topic feed
+function populateTopicFeed(topicFeedLength, jsonData, topic){
+    
+    var feeds = [];
+    
+    feeds = getFeedsByTopic(topicFeedLength, jsonData, topic);
+    
+    for ( var i = 0; i < topicFeedLength; i++ ) {
+        j = i+1;
+        
+        //Empty previous feeds
+        $("#rss-feed"+j).find("div").html("");
+        $("#rss-feed"+j).find("h2").remove();
+
+        //Populate Headings
+        parseFeed(feeds[i], '#rss-feed'+j);
+
+        $("#rss-feed"+j).rss(
+
+            //Change this to your own rss feeds
+            feeds[i],
+
+            {
+            // how many entries do you want?
+            // default: 4
+            // valid values: any integer
+            limit: feedCount,
+
+            // the effect, which is used to let the entries appear
+            // default: 'show'
+            // valid values: 'show', 'slide', 'slideFast', 'slideSynced', 'slideFastSynced'
+            effect: 'slideFastSynced',
+
+            // outer template for the html transformation
+            // default: "<ul>{entries}</ul>"
+            // valid values: any string
+            layoutTemplate: '<div class="">{entries}</div>',
+
+            // inner template for each entry
+            // default: '<li><a href="{url}">[{author}@{date}] {title}</a><br/>{shortBodyPlain}</li>'
+            // valid values: any string
+            entryTemplate: entryTemp
+            }
+        );
+    }
+}
+
+function clearPriorContent(){
+    $("#rss-feed0").find("div").remove();
+    $("#rss-feed0").find(".divider").remove();
+}
 
 
 
